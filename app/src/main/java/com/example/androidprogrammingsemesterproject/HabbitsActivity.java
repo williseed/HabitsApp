0e71adc006532e7f148f8de733f1baad030c31f8
ZacharyHartman445 <zhartma445@gmail.com>
2022-04-02 23:18:23 -0500

    Added button, onclicks

--- app/src/main/java/com/example/androidprogrammingsemesterproject/HabbitsActivity.java
+++ app/src/main/java/com/example/androidprogrammingsemesterproject/HabbitsActivity.java
@@ -3,6 +3,8 @@ package com.example.androidprogrammingsemesterproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HabbitsActivity extends AppCompatActivity {
 
@@ -10,5 +12,37 @@ public class HabbitsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habbits);
+
+        Button buttonAdd = (Button)findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button buttonRemove = (Button)findViewById(R.id.buttonRemove);
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button buttonEdit = (Button)findViewById(R.id.buttonEdit);
+        buttonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button buttonHome = (Button)findViewById(R.id.buttonHome);
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
