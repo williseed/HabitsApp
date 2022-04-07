0a8d572026f3eb2b8756a0676bb714931b436af7
williseed <williamsbosch@yahoo.com>
2022-04-07 16:58:37 -0500

    Added Practice fragment to home activity so my login will go to home page after successful login

--- app/src/main/java/com/example/androidprogrammingsemesterproject/HomeActivity.java
+++ app/src/main/java/com/example/androidprogrammingsemesterproject/HomeActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
-
+    HomeFragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        fragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container,fragment);
     }
 }
