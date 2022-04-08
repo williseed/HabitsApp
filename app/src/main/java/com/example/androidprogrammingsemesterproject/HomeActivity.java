da503fee21925e598ce0f617384f2f2aa764ddc7
De-Shian Lin <dxl93320@ucmo.edu>
2022-04-08 07:28:40 -0500

    Home Page:   'View Progress' and 'Modify Habbits' buttons work   Action bar added
    Changed files:
      HomeActivity.java
        - link button 'View Progress' --(intent)--> ProgressActivity.class
        - link button 'Modify Habbits' --(intent)--> HabbitsActivity.class
        - added the action bar
      activity_home.xml
        - modified buttons' ids

--- app/src/main/java/com/example/androidprogrammingsemesterproject/HomeActivity.java
+++ app/src/main/java/com/example/androidprogrammingsemesterproject/HomeActivity.java
@@ -2,15 +2,53 @@ package com.example.androidprogrammingsemesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    // declare the fragment
     HomeFragment fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // put the fragment in the container
         fragment = new HomeFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.container,fragment);
+
+        // a button links to ProgressActivity.class // by intent
+        Button buttonViewProgress = (Button)findViewById(R.id.buttonViewProgress);
+        buttonViewProgress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProgressActivity.class);
+                startActivity(intent);
+            }
+        }); // end buttonViewProgress
+
+        // a button links to HabbitsActivity.class // by intent
+        Button buttonModifyHabbits = (Button)findViewById(R.id.buttonModifyHabbits);
+        buttonModifyHabbits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HabbitsActivity.class);
+                startActivity(intent);
+            }
+        }); // end buttonModifyHabbits
+
+    } // end onCreate(Bundle savedInstanceState)
+
+    // action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
-}
+
+} // end HomeActivity
